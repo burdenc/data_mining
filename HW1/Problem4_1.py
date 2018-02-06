9280787e418710131cc2207a919d4c52f451210d
Ryan Meek <ryanjmeek6@gmail.com>
2018-02-05 21:06:02 -0600

    residuals plot included now

--- HW1/Problem4_1.py
+++ HW1/Problem4_1.py
@@ -4,18 +4,17 @@ import csv
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-print("Hello")
 
 filename = "dow_jones_index.data"
-#file = open(filename, "r")
 i = 0
 data = []
 target = []
+x = []
+num = 0
 with open(filename, 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         if i != 0:
-            print("row[8]: ", row[8], " row[9]: ", row[9], " row[11]: ", row[11])
             if len(row[8]) == 0: continue
             if len(row[9]) == 0: continue
             row[11] = row[11].replace("$","")
@@ -23,17 +22,24 @@ with open(filename, 'r') as csvfile:
             sample = [float(row[8]), float(row[9])] #row[8]: percent_change_price, row[9]: percent_change_volume_over_last_wk
             data.append(sample) #percent_change_price
             target.append(float(row[11])) #next_weeks_open     
+            x.append(num)
+            num = num + 1
         i = i + 1
-    print("data[0]: ", data, " target[0]", target)
     reg = linear_model.LinearRegression()
     reg.fit(data,target)
-    print("1 Here are coef: ", reg.coef_ )
+    print("Here are coefficients: ", reg.coef_ )
     predictions = reg.predict(data)
-    print("Mean squared error: %.2f" , mean_squared_error(target, predictions))
-    #LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
-    #print("2 Here are coef: ", reg.coef_ )
-    #reg.fit(data,target)
-    #print("3 Here are coef: ", reg.coef_ )
-      	 
+    #print("target: ", target)
+    #print("predictions: ", predictions)
+    print("Mean squared error: " , mean_squared_error(target, predictions))
+    residuals = np.subtract(target,predictions)
+    residuals = np.absolute(residuals)
+    plt.plot(x,residuals)
+    plt.ylabel("Residual")
+    plt.xlabel("Sample Number")
+    plt.show()
+    #print("x: ", x)
+    #print("residuals: ", residuals)
+          	 
             
 
